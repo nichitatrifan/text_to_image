fa4230f6728fb25eafac0595dbb3e2da7445e232
nichitatrifan <nikiton254@gmail.com>
2022-01-17 15:02:33 -0600

    fixing the venv

--- server.py
+++ server.py
@@ -2,74 +2,81 @@ from email.errors import NonPrintableDefect
 import socket
 import threading
 import json
+import logging
 
 from number import generate_prime_number, generate_random_number, N_SIZE
+from settings import *
 
 
-HEADER = 1024
-PORT = 5050
-HOST = 'localhost'
-ADDR = (HOST, PORT)
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = '!DISCONNECT!'
+class ThreadServer:
+    def __init__(self) -> None:
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind(ADDR)
+        logging.basicConfig()
+        self.logger = logging.getLogger('TCPLogger')
+        self.logger.setLevel(logging.INFO)
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDR)
+    def handle_client(self, client_socket, addr):
+        print(f'[NEW CONNECTION] {addr} connected.')
+        connected = True
+        
+        while connected:
+            try:
+                data = json.loads(client_socket.recv(1024).strip().decode(FORMAT))
+            except:
+                self.logger.warning('[NO DATA HAS BEEN SENT]')
+                connected = False
+                data = None
 
-def handle_client(conn, addr):
-    print(f'[NEW CONNECTION] {addr} connected.')
-    connected = True
-    
-    while connected:
-        try:
-            data = json.loads(conn.recv(1024).strip().decode(FORMAT))
-        except:
-            print('[NO DATA HAS BEEN SENT]')
-            connected = False
-            data = None
+            if data:
+                # msg_length = int(msg_length)
+                # msg = conn.recv(msg_length).decode(FORMAT)
+                # if msg == DISCONNECT_MESSAGE:
+                #     connected = False
+                # print(f'[{addr}] {msg}')
+                
+                # print(data)
+                
+                b = []
+                B = []
+                pixels = []
 
-        if data:
-            # msg_length = int(msg_length)
-            # msg = conn.recv(msg_length).decode(FORMAT)
-            # if msg == DISCONNECT_MESSAGE:
-            #     connected = False
-            # print(f'[{addr}] {msg}')
-            print(data)
-            b = []
-            B = []
-            pixels = []
+                for n,h in zip(data['n'], data['h']):
+                    bb = generate_prime_number(N_SIZE)
+                    b.append(bb)
+                    B.append(pow(n,bb,h))
 
-            for n,h in zip(data['n'], data['h']):
-                bb = generate_prime_number(N_SIZE)
-                b.append(bb)
-                B.append(pow(n,bb,h))
+                return_data = { 
+                    'B': B
+                    }
 
-            return_data = { 'B': B }
-            conn.sendall(f'{json.dumps(return_data)}\n'.encode(FORMAT)) # encodes to a byte array
-            
-            B_prime = []
-            for A,bb,h in zip(data['A'], b, data['h']):
-                B_prime.append(pow(A,bb,h) % 200)
-            pixels.append(tuple(B_prime))
+                client_socket.sendall(f'{json.dumps(return_data)}\n'.encode(FORMAT)) # encodes to a byte array
+                
+                B_prime = []
+                for A,bb,h in zip(data['A'], b, data['h']):
+                    B_prime.append(pow(A,bb,h) % 200)
+                pixels.append(tuple(B_prime))
 
-            if B_prime:
-                print(f'{addr} added RGB value: {B_prime}')
+                if B_prime:
+                    self.logger.info(f'{addr} added RGB value: {B_prime}')
 
-    conn.close()
+        client_socket.close()
 
-def start():
-    server.listen()
-    print(f'[LISTENNING] Server is listenning on {HOST} {PORT}')
-    while True:
-        try:
-            client_socket, addr = server.accept() # it waits here for a new connection
-            thread = threading.Thread(target=handle_client, args=(client_socket, addr))
-            thread.start()
-            print(f'[ACTIVE CONNECTIONS] {threading.activeCount() - 1}')
-        except KeyboardInterrupt as kyi:
-            print('[KeyBoard Interrupt]')
-            server.close()
+    def start(self):
+        self.server.listen()
+        self.logger.info(f'[LISTENNING] Server is listenning on {HOST} {PORT}')
+
+        while True:
+            try:
+                client_socket, addr = self.server.accept() # it waits here for a new connection
+                thread = threading.Thread(target=self.handle_client, args=(client_socket, addr))
+                thread.start()
+                self.logger.info(f'[ACTIVE CONNECTIONS] {threading.activeCount() - 1}')
+            except KeyboardInterrupt as kyi:
+                self.logger.warning('[KeyBoard Interrupt]')
+                self.server.close()
 
 if __name__ == '__main__':
     print('Starting the server at: ' + str(HOST) + ' ' + str(PORT))
-    start()
+    my_server = ThreadServer()
+    my_server.start()
