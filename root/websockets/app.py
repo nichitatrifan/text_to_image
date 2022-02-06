17431b0442dff126b6713e6832dc6088b8e6b797
nichitatrifan <nikiton254@gmail.com>
2022-02-05 18:03:42 -0600

    add to commit

---
+++ root/websockets/app.py
@@ -0,0 +1,19 @@
+import asyncio
+import websockets
+
+# handler that manages each connection
+async def handler(websocket):    
+    await websocket.send('Hello User!')
+    async for message in websocket:
+        print(message)
+
+
+async def main():
+    async with websockets.serve(handler, "", 8001):
+        await asyncio.Future()  # run forever
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt as ex:
+            print('KeyboardInterrupt')
